4ddb0febe547e4132b893e7e7610ddc8d7ab1d48
DatAKAjezz <jezzthedead@gmail.com>
2024-12-30 01:34:04 +0700

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import { FaRegBell, FaRegFolderOpen } from 'react-icons/fa'
+import { HiMenu } from 'react-icons/hi'
+import { RiFilePaperLine, RiHome9Line } from 'react-icons/ri'
+import '../index.css'
+import { TbCategory } from 'react-icons/tb'
+import { useState } from 'react'
 
 const Header = () => {
-  return (
-    <div>
-      toi la header
+  const [isOpened, setIsOpened] = useState<boolean>(false);
+
+  const menuItems = [
+    { icon: <RiHome9Line className='text-3xl mr-4' />, label: 'Trang chủ' },
+    { icon: <FaRegFolderOpen className='text-3xl mr-4' />, label: 'Thư viện' },
+    { icon: <FaRegBell className='text-3xl mr-4' />, label: 'Thông báo' },
+    { icon: <RiFilePaperLine className='text-3xl mr-4' />, label: 'Thẻ ghi nhớ' },
+    { icon: <TbCategory className='text-3xl mr-4' />, label: 'Lời giải chuyên gia' },
+  ];
+
+  return (  
+    <div className={`font-bold text-base flex flex-col ${isOpened ? 'w-1/6' : 'w-16'} pr-4 box-content
+                     text-white transition-all duration-300 ease-in`}
+         style={{backgroundColor: 'rgb(21, 18, 20)', height: '100vh'}}>
+      <div className='flex w-fit flex-row items-center align-middle mt-2'>
+        <HiMenu className='ml-4 text-4xl cursor-pointer'
+                onClick={() => setIsOpened(prev => !prev)} />
+      </div>
+      <ul>
+        {menuItems.slice(0, 3).map((item, index) => (
+          <li key={index} className='custom-menu-li'>
+            {item.icon}
+            <span className={`menu-item-label ${isOpened ? 'open' : ''}`}>{item.label}</span>
+          </li>
+        ))}
+      </ul>
+      <hr className='ml-2 w-full mb-4'/>
+      <ul>
+        {menuItems.slice(3).map((item, index) => (
+          <li key={index} className='custom-menu-li'>
+            {item.icon} 
+            <span className={`menu-item-label ${isOpened ? 'open' : ''}`}>{item.label}</span>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
